be0012c933ce727ac3b81b6c1cc394059b090426
p13l <p-e.anki@pm.me>
2024-07-31 18:56:49 +0200

    ajout de commentaires

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import extract
 import transform
 
 def main():
+    # Extraction des urls catégories depuis la homepage
+
+
+
+    # Extraction des urls des produits depuis la liste d'urls de chaque catégorie
+
+
     print("yy")
     product_info = extract.extract_information_of_a_product_page('https://books.toscrape.com/catalogue/howl-and-other-poems_522/index.html')
     print(product_info)
